a33517ae821c5f7b43188cec3d5c1065f61bf97d
khalifeh <khalifeh.dev@gmail.com>
2026-07-08 18:10:01 +0330

    feat(main): add initial setting to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+
+    // External Modules
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,42 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import cookieParser from "cookie-parser"
 import { AppModule } from './app.module';
 
 (async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService)
+
+  app.use(cookieParser())
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1",
+    prefix: "api/v"
+  })
+  
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "http://localhost:5500",
+    ],
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTION"],
+    credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+    ],
+    exposedHeaders: ["X-Total-Count"],
+    maxAge: 3600
+  })
+
+  const appPort = configService.get("APP_PORT", 9001)
+
+  await app.listen(appPort, () => {
+    console.log(`🚀 Application is running on: http://localhost:${ appPort }/api/v1`)
+  });
 })()
